944c703422b2fb90a46c4039116f7e9c657bc04d
Dom McCabe <dommct@yahoo.co.uk>
2021-02-17 11:17:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-data = fetch("https://market.fuzzwork.co.uk/aggregates/?region=10000002&types=34,35,36,37,38,39,40")
-.then(response => response.json)
+fetch("https://market.fuzzwork.co.uk/aggregates/?region=10000002&types=34,35,36,37,38,39,40")
+.then(response => response.json())
+.then((json) => {
+console.log(json)
+})
