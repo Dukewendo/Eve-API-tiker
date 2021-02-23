e74a250b9dd04ce69897ead6f89681be26e7471d
Dom McCabe <dommct@yahoo.co.uk>
2021-02-23 11:40:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const getData = async function() {
 try {
+  
   const resp = await fetch("https://market.fuzzwork.co.uk/aggregates/?region=10000002&types=34,35,36,37,38,39,40")
   const data = await resp.json();
   // CODE here to loop through data and assign to HTML LIST
   
   //loop through [mineral] + LOOP THROUGH OBJECTS
 
-minerals.forEach((num1, index) => {
-  const num2 =data[index];
-  console.log(num1, num2);
-});
-
-//console.log(minerals[0] + " " + data["34"].buy.max + " ISK")
+var x;
+for ( x in data) {
+  document.getElementById("ticker").innerHTML +=(data[x].buy.max);
+}
   
-   
+
      
 } catch (err){
   console.log("Something went wrong!", err);
