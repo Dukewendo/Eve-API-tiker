d1d9278f207fcc17bfa04f06059e3abb14fdd0a5
Dom McCabe <dommct@yahoo.co.uk>
2021-02-23 10:09:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-fetch("https://market.fuzzwork.co.uk/aggregates/?region=10000002&types=34,35,36,37,38,39,40")
-.then(response => response.json())
-.then((json) => {
-console.log(json)
-})
+const getData = async function() {
+try {
+  const resp = await fetch("https://market.fuzzwork.co.uk/aggregates/?region=10000002&types=34,35,36,37,38,39,40")
+  const data = await resp.json();
+  // CODE here to loop through data and assign to HTML LIST
+  
+  //loop through [mineral] + LOOP THROUGH OBJECTS
+
+minerals.forEach((num1, index) => {
+  const num2 =data[index];
+  console.log(num1, num2);
+});
+
+//console.log(minerals[0] + " " + data["34"].buy.max + " ISK")
+  
+   
+     
+} catch (err){
+  console.log("Something went wrong!", err);
+}
+}
+
+getData();
+
+let minerals= [
+"Tritanium",
+"Pyerite",
+"Mexallon",
+"Isogen",
+"Nocxium",
+"Zydrine",
+"Megacyte"
+]
